8e668d0315d9b0359e3ba299f550d4a533074230
Sebastian Urban <surban@surban.net>
2017-10-06 01:29:02 +0200

    initial commit

---
+++ FourierMotzkin.fs
@@ -0,0 +1,136 @@
+/// Foruier-Motzkin elimination method for solving system of inequalities.
+/// This algorithm is only practical for very small systems.
+module FourierMotzkin
+
+open Tensor
+
+
+/// A solution to a system of inequalities.
+/// Use 'range' to obtain valid ranges and 'subst' to substitute a value for a variable.
+type Solution = {
+    /// list of left sides of reduced systems
+    ReducedA:   Tensor<Rat> list
+    /// list of right sides of reduced system
+    ReducedB:   Tensor<Rat> list
+    /// set of variable indicies that are arbitrary
+    Arbitrary:  Set<int64>
+    /// variable values for backsubstitution
+    Values:     Rat list
+}
+
+
+/// Solves a system of inequalities of the form A .* x >= b.
+/// After the solution is computed, use the functions 'range' and 'subst' to obtain the allowed
+/// ranges of the variables and substitute values for them in the inequality system.
+/// Returns None if the inequality system is infeasible.
+let solve (A: Tensor<Rat>) (b: Tensor<Rat>) =   
+    let m, n = A.Shape.[0], A.Shape.[1]
+    let rec eliminate (rA: Tensor<Rat>) (rb: Tensor<Rat>) rAs rbs arb =
+        let k = List.length rAs |> int64
+        //printfn "Elimination step %d:" k
+        //printfn "rA=\n%A" rA
+        //printfn "rb=\n%A" rb
+        //printfn "arbitrary: %A" arb
+        if k < n then
+            let rA, rb = Tensor.copy rA, Tensor.copy rb
+            let zRows = rA.[*, k] ==== Rat.Zero
+            let nzRows = ~~~~zRows
+
+            // Divide rows so that x_k = +1 or x_k = -1 or x_k = 0 in each inequality.     
+            let facs = abs (rA.Masked(nzRows, NoMask).[*, k]) 
+            rA.Masked(nzRows, NoMask) <- rA.Masked(nzRows, NoMask) / facs.[*, NewAxis]
+            rb.Masked(nzRows) <- rb.Masked(nzRows) / facs
+
+            //printfn "after division:"
+            //printfn "rA=\n%A" rA
+            //printfn "rb=\n%A" rb
+
+            // Check condition of x_k.
+            if Tensor.all (rA.[*, k] ==== Rat.Zero) then
+                // all the coefficients of x_k are zero, thus it is arbitrary
+                //printfn "all x_k=0"
+                eliminate rA rb (rA::rAs) (rb::rbs) (arb |> Set.add k)
+            elif Tensor.all (rA.[*, k] ==== Rat.One) || Tensor.all (rA.[*, k] ==== Rat.MinusOne) then
+                // the coefficients of x_k are all +1 or -1
+                //printfn "all x_k=+1 or all x_k=-1"
+                eliminate (rA.Masked(zRows, NoMask)) (rb.Masked(zRows)) 
+                          (rA::rAs) (rb::rbs) (arb |> Set.union (Set [k+1L .. n-1L]))
+            elif Tensor.all ((rA.[*,k] ==== Rat.Zero) |||| (rA.[*,k] ==== Rat.One)) ||
+                 Tensor.all ((rA.[*,k] ==== Rat.Zero) |||| (rA.[*,k] ==== Rat.MinusOne)) then
+                // the coefficients of x_k are a mix of 0 and +1 or a mix of 0 and -1
+                //printfn "x_k is mix of 0 and +1 or mix of 0 and -1"
+                eliminate (rA.Masked(zRows, NoMask)) (rb.Masked(zRows)) (rA::rAs) (rb::rbs) arb
+            else
+                //printfn "x_k has +1 and -1"
+                // there is at least one pair of inequalities with a +1 and a -1 coefficient for x_k
+                let pRows = rA.[*, k] ==== Rat.One |> Tensor.trueIdx |> Tensor.flatten |> HostTensor.toList
+                let nRows = rA.[*, k] ==== Rat.MinusOne |> Tensor.trueIdx |> Tensor.flatten |> HostTensor.toList
+                // for each pair augment the reduced system by their sum
+                let nextRA = 
+                    List.allPairs pRows nRows            
+                    |> List.map (fun (p, n) -> rA.[p..p, *] + rA.[n..n, *])
+                    |> List.append [rA.Masked(zRows, NoMask)]
+                    |> Tensor.concat 0
+                let nextRb = 
+                    List.allPairs pRows nRows            
+                    |> List.map (fun (p, n) -> rb.[p..p] + rb.[n..n])
+                    |> List.append [rb.Masked(zRows)]
+                    |> Tensor.concat 0      
+                eliminate nextRA nextRb (rA::rAs) (rb::rbs) arb
+        else
+            // Feasibility check
+            if Tensor.any (rb >>>> Rat.Zero) then None
+            else 
+                Some {
+                    ReducedA = rAs
+                    ReducedB = rbs
+                    Arbitrary = arb
+                    Values = []
+                }
+
+    eliminate A b [] [] Set.empty
+
+
+/// Gets the index j of the active variable x_j.
+/// Returns -1L if no variable is active, i.e. all variables have been substituted.
+/// A solution returned by 'solve' starts with the last element of x being active.
+/// Each call to 'subst' moves forward by one element.
+let active (sol: Solution) =
+    int64 sol.ReducedA.Length - 1L
+
+
+/// Gets the allowed range of active variable.
+/// Returns a tuple (low, high).
+let range (sol: Solution) =
+    let j = active sol
+    if j < 0L then failwith "no variable active"
+    if sol.Arbitrary |> Set.contains j then
+        Rat.NegInf, Rat.PosInf
+    else
+        //printfn "Calculating range for x_%d:" j
+        // substitute the values into the system: x = [0; ...; 0; v_j; ...; v_n]
+        let A, b = List.head sol.ReducedA, List.head sol.ReducedB
+        //printfn "A=\n%A" A
+        //printfn "b=\n%A" b
+        let x = Tensor.concat 0 [HostTensor.zeros [j+1L]; HostTensor.ofList sol.Values] 
+        //printfn "x=\n%A" x
+        let s = b - A .* x
+        //printfn "s=\n%A" s
+        // if coefficient of x_j is +1, then line of s is lower limit for x_j
+        // if coefficient of x_j is -1, then line of -s is upper limit for x_j
+        let low = s.Masked(A.[*,j] ==== Rat.One) |> Tensor.max
+        let high = -s.Masked(A.[*,j] ==== Rat.MinusOne) |> Tensor.min
+        low, high
+
+
+/// Substitutes the active variable x_j with the given value, which must be within its allowed range,
+/// and makes the variables x_{j-1} active.
+let subst (value: Rat) (sol: Solution) =
+    let low, high = range sol
+    if not (low <= value && value <= high) then
+        failwithf "value %A out of required range: %A <= x_%d <= %A" value low (active sol) high
+    {sol with
+        ReducedA = List.tail sol.ReducedA
+        ReducedB = List.tail sol.ReducedB
+        Values = value :: sol.Values}
+
---
+++ Program.fs
@@ -0,0 +1,551 @@
+﻿
+open System
+open System.Numerics
+open PLplot
+open Tensor
+open Tensor.Algorithms
+
+
+
+
+let printMat title mat =
+    let prefix = title + " = "
+    let mp = sprintf "%A" mat
+    let mp = mp.Replace("\n", "\n" + String.replicate prefix.Length " ")
+    printfn "%s%s" prefix mp
+
+
+let rectangle (pl: PLStream) (x1, y1) (x2, y2) =
+    pl.line ([|x1; x2; x2; x1; x1|], [|y1; y1; y2; y2; y1|])
+
+let gcd a b =
+    BigInteger.GreatestCommonDivisor (a, b)
+
+let lcm a b =
+    (a * b) / gcd a b |> abs
+
+
+let test1 () =
+
+    let xBox = 7.0
+    let yBox = 5.0
+
+    let M = [[0; 0]]
+    //let P = [3]
+
+    let M = M |> HostTensor.ofList2D
+
+    use pl = new PLStream()
+    pl.sdev "xcairo"
+    pl.setopt("geometry", "1280x1024") |> ignore
+    pl.init()
+    pl.env(-8.0, 8.0, -6.0, 6.0, AxesScale.Independent, AxisBox.BoxTicksLabels)
+    pl.lab("x", "y", "")
+
+    // draw lattice
+    let x = HostTensor.arange -xBox 1.0 xBox 
+    let y = HostTensor.arange -yBox 1.0 yBox 
+    let xg = Tensor.replicate 1 y.Shape.[0] x.[*, NewAxis] |> Tensor.flatten |> HostTensor.toArray
+    let yg = Tensor.replicate 0 x.Shape.[0] y.[NewAxis, *] |> Tensor.flatten |> HostTensor.toArray
+    pl.col0 Color.Grey
+    pl.poin (xg, yg, Symbol.Dot)
+    rectangle pl (xBox, yBox) (-xBox, -yBox)
+
+    // // calculate matrix inverse
+    // let M = M |> HostTensor.ofList2D |> Tensor.convert<Rat>
+    // let I, S, N = LinAlg.generalInverse M
+    // let _, SN, _ = LinAlg.generalInverse N
+    
+    // printMat "matrix M" M
+    // printMat "inverse I" I
+    // printMat "solvability S" S
+    // printMat "nullspace N" N
+    // printMat "solvability of nullspace SN" SN
+    // printfn ""
+
+    // // bring nullspace in integer form by multiplying each row with the LCM of its denominators
+    // let LCM = SN |> Tensor.foldAxis (fun l x -> lcm l x.Dnm) (HostTensor.scalar (bigint 1)) 1
+    // let SN = SN * (Tensor.convert<Rat> LCM.[*, NewAxis]) |> Tensor.convert<bigint>
+    // printMat "integer solvability of nullspace SN" SN
+
+    // // calculate Smith normal form of nullspace
+    // let SNU, SNS, SNV = LinAlg.smithNormalForm SN
+    // printMat "SNU" SNU
+    // printMat "SNS" SNS
+    // printMat "SNV" SNV
+
+    // calculate integer inverse of M
+    printfn "integer inversion:"
+    let I, S, N = LinAlg.integerInverse (Tensor.convert<bigint> M)
+    printMat "inverse I" I
+    printMat "solvability S" S
+    printMat "nullspace N" N   
+
+
+    // plot nullspace lattice
+    let plotNullspacePoints x0 =
+        let zMin, zMax = bigint -5, bigint 5
+        let rec doPlot (z: Tensor<bigint>) =
+            let x = x0 + N .* z |> Tensor.convert<float>
+            pl.col0 Color.BlueViolet
+            let lbl = z |> Tensor.convert<int> |> HostTensor.toList |> List.map (sprintf "%d") |> String.concat ""
+            pl.string2 ([|x.[[0L]]|], [|x.[[1L]]|], lbl)
+
+            let rec incr pos =
+                if pos >= z.NElems then false
+                else
+                    if z.[[pos]] = zMax then 
+                        z.[[pos]] <- zMin
+                        incr (pos+1L)
+                    else
+                        z.[[pos]] <- z.[[pos]] + bigint.One
+                        true
+            if incr 0L then doPlot z                    
+        doPlot (HostTensor.filled [N.Shape.[1]] zMin)
+
+
+    let ys = [[-2]; [-1]; [0]; [1]; [2]] 
+    for y in ys do
+        let y = HostTensor.ofList y |> Tensor.convert<bigint>        
+        printfn "y point: %A" y
+        if S .* y ==== bigint.Zero |> Tensor.all then
+            printfn "fulfilles solvabilty constraint"
+            let x0 = I .* Tensor.convert<Rat> y
+            if x0 |> Tensor.map Rat.isInteger |> Tensor.all then
+                printfn "produces integer solution"
+                let grp = sprintf "%A" y
+                pl.schr(0.0, 0.7)
+                plotNullspacePoints (Tensor.convert<bigint> x0)
+            else
+                printfn "does not produce integer solution"
+        else
+            printfn "does not fulfill solvability contraint"         
+    
+
+    //exit 0
+
+    // for p in [0; 1; 2; 3] do
+    //     let P = [p]
+    //     // invert point P
+    //     let P = P |> HostTensor.ofList |> Tensor.convert<Rat>
+    //     let R = I .* P
+
+    //     //printMat "point P" P
+    //     //printMat "inverse of point R" R
+
+    //     // so we have the inverse point and the nullspace vector
+
+    //     // draw R point
+    //     let Rx, Ry = float R.[[0L]], float R.[[1L]]
+    //     pl.col0 Color.Red
+    //     pl.poin ([|Rx|], [|Ry|], 'x')
+
+    //     // draw base vectors of null-space of M
+    //     let f=10.
+    //     let Nx, Ny = float N.[[0L; 0L]], float N.[[1L; 0L]]
+    //     pl.col0 Color.BlueViolet
+    //     pl.line ([|Rx-f*Nx; Rx; Rx+f*Nx|], [|Ry-f*Ny; Ry; Ry+f*Ny|])
+
+    //     // draw orthgonal base vectors of null-space of R
+    //     let f=0.5
+    //     let SNx, SNy = float SN.[[0L; 0L]], float SN.[[0L; 1L]]
+    //     pl.col0 Color.Magenta
+    //     pl.line ([|Rx; Rx+f*SNx|], [|Ry; Ry+f*SNy|])        
+
+
+let test2() =
+    let M = [[2; 1; 3];
+             [1; 2; 3];
+             [3; 3; 6]]
+    let M = M |> HostTensor.ofList2D |> Tensor.convert<Rat>
+    let A = HostTensor.identity 3L
+
+    let R, _, _, B = LinAlg.rowEchelonAugmented M A
+
+    printMat "M" M
+    printMat "A" A
+    printMat "row echelon R" R
+    printMat "inverse B" B
+
+    // now the row echelon is already integer but the inverse is not
+
+    // calculate the LCM of the denominators of all rows
+    //or row in 0L .. M.Shape.[0] - 1L do
+    let LCMR = R |> Tensor.foldAxis (fun l x -> lcm l x.Dnm) (HostTensor.scalar (bigint 1)) 1
+    let LCMRB = B |> Tensor.foldAxis (fun l x -> lcm l x.Dnm) LCMR 1
+
+    printMat "LCM AL" LCMRB
+
+    // multiply the rows accordingly
+    let LCMRB = LCMRB |> Tensor.convert<Rat> |> Tensor.padRight
+    let R = R * LCMRB
+    let B = B * LCMRB
+
+    printfn "after multiplication with LCM:"
+    printMat "row echelon R" R
+    printMat "inverse B" B
+    
+    // next step:
+    // understand the meaning of R and B now
+
+
+let test3() =
+    //let M = [[10; 3; 3; 8];
+    //         [6; -7; 0;-5]]
+    let M = [[0; 0; 3; 8];
+             [0; 0; 0;-5]]             
+    let M = M |> HostTensor.ofList2D |> Tensor.convert<Rat>
+
+    let I, S, N = LinAlg.generalInverse M
+   
+    printMat "matrix M" M
+    printMat "inverse I" I
+    printMat "solvability S" S
+    printMat "nullspace N" N
+
+let test4() =
+    let M = [[2; 1; 3];
+             [1; 2; 3];
+             [3; 3; 6]]
+    let M = M |> HostTensor.ofList2D |> Tensor.convert<bigint>
+    let U, S, V = LinAlg.smithNormalForm M
+
+    printMat "matrix M" M
+    printMat "U" U
+    printMat "Smith S" S
+    printMat "V" V
+
+
+let setupPlot (pl: PLStream) xRng yRng =
+    pl.sdev "xcairo"
+    pl.setopt("geometry", "1280x1024") |> ignore
+    pl.init()
+    pl.env(-xRng, xRng, -yRng, yRng, AxesScale.Independent, AxisBox.BoxTicksLabels)
+    pl.lab("x", "y", "")
+
+let drawLattice (pl: PLStream) xRng yRng =
+    let x = HostTensor.arange -xRng 1.0 xRng
+    let y = HostTensor.arange -yRng 1.0 yRng 
+    let xg = Tensor.replicate 1 y.Shape.[0] x.[*, NewAxis] |> Tensor.flatten |> HostTensor.toArray
+    let yg = Tensor.replicate 0 x.Shape.[0] y.[NewAxis, *] |> Tensor.flatten |> HostTensor.toArray
+    pl.col0 Color.Grey
+    pl.poin (xg, yg, Symbol.Dot)    
+
+let drawPolygon (pl: PLStream) (polygon: Tensor<float>) =
+    let x = polygon.[0L, *] |> HostTensor.toList
+    let y = polygon.[1L, *] |> HostTensor.toList    
+    pl.line(x @ [x.Head] |> List.toArray, y @ [y.Head] |> List.toArray)
+
+let testRectTransform() =
+    let xRng, yRng = 10., 10.
+    use pl = new PLStream()
+    setupPlot pl xRng yRng
+    drawLattice pl xRng yRng
+
+    // generate rectangle points
+    let genBox (xMin, yMin) (xMax, yMax) =
+        // upper line
+        let ux = [xMin .. xMax]
+        let uy = List.replicate ux.Length yMax
+        // right line
+        let ry = [yMax .. -1 .. yMin]
+        let rx = List.replicate ry.Length xMax
+        // lower line
+        let dx = [xMax .. -1 .. xMin]
+        let dy = List.replicate dx.Length yMin
+        // left line
+        let ly = [yMin .. yMax-1]
+        let lx = List.replicate ly.Length xMin
+
+        let x = ux @ rx @ dx @ lx |> HostTensor.ofList
+        let y = uy @ ry @ dy @ ly |> HostTensor.ofList
+        Tensor.concat 0 [x.[NewAxis, *]; y.[NewAxis, *]]
+
+    let box = genBox (-3, -2) (2, 1)
+    pl.col0 (Color.Aquamarine)
+    drawPolygon pl (Tensor.convert<float> box)
+
+    // transform box into basis space
+    let B = [[1; 0]
+             [3;-1]]
+    let B = HostTensor.ofList2D B
+    let IB, _, _ = LinAlg.integerInverse (Tensor.convert<bigint> B)
+    let IB = Tensor.convert<int> IB
+    printMat "Basis    B" B
+    printMat "Inverse IB" IB
+    printMat "IB .* B" (IB .* B)
+
+    let bbox = IB .* box
+    pl.col0 (Color.Salmon)
+    drawPolygon pl (Tensor.convert<float> bbox)
+
+
+    printMat "box" box
+    printMat "bbox" bbox
+
+    ()
+
+
+
+type DimFaith =
+    /// Dimension is free with given index
+    | Free of int
+    /// Dimension is dependant with given index
+    | Dependant of int
+
+
+type SumInfo = {
+    // xFree are the summation coordinates.
+    // xDep = XFreeToXDep .* XFree + YToXDep .* Y
+    // x = [xFree; xDep]
+
+    /// Solvability .* Y = 0 for system to be solvable
+    Solvability:        Tensor<bigint>
+
+    /// What has become of a coordinate?
+    XDimFaith:          DimFaith list
+
+    /// Matrix mapping from free to dependant coordinates
+    XFreeToXDep:       Tensor<Rat>
+
+    /// Matrix mapping from input coordinates to dependant coordinates
+    YToXDep:            Tensor<Rat>
+
+    // Constraints 1 are of the form:
+    // XFreeToC .* xFree + YToC .* Y >= cb
+
+    /// Matrix mapping from free coordinates to constraints
+    XFreeToC1:          Tensor<Rat> 
+
+    /// Matrix mapping from input coordinates to constraints
+    YToC1:              Tensor<Rat>
+
+    /// Constraint bias 
+    CB1:                Tensor<int64> 
+
+    // Constraints 2 are of the form:
+    // XFreeToC .* xFree + YToC .* Y <= cb
+
+    /// Matrix mapping from free coordinates to constraints
+    XFreeToC2:          Tensor<Rat> 
+
+    /// Matrix mapping from input coordinates to constraints
+    YToC2:              Tensor<Rat>
+
+    /// Constraint bias 
+    CB2:                Tensor<int64>     
+
+    // Divisibility constraints are of the form:
+    // XFree = YToR .* Y (mod RDiv)
+
+    /// Matrix mapping from input coordinates to division remainders
+    YToRDiv:            Tensor<Rat>
+
+    /// Dividers
+    RDiv:               Tensor<bigint>       
+}
+
+
+type InputRange = int64 * int64
+
+
+let test5() =
+
+
+    // let M = [[2; 1; 3];
+    //          [1; 2; 3];
+    //          [3; 3; 6]]
+    //let M = [[1; 2; 3];
+    //         [2; 4; 6];
+    //         [3; 1; 6]]      
+    let M = [[1; 2; 3];
+             [1; 2; 3];
+             [1; 2; 3]]      
+
+    // Y = M .* X
+
+    let XRanges = [(0L, 10L); (0L, 20L); (0L, 30L)]
+
+
+    let M = M |> HostTensor.ofList2D |> Tensor.convert<bigint>
+    let I, S, N = LinAlg.integerInverse M
+
+
+    let nOut, nIn = M.Shape.[0], M.Shape.[1]
+
+
+    printMat "M" M
+    printMat "inverse   I" I
+    printMat "nullspace N" N
+
+    // so N is the nullspace basis matrix
+    let dNull = N.Shape.[1]
+
+    // We need as many input dimensions as there are columns, the remaining dimensions will become dependants.
+    // By removing rows with minimum absolute value first, it is ensured that zero rows are always removed.
+    let dependants = 
+        N 
+        |> abs 
+        |> Tensor.convert<Rat>
+        |> Tensor.minAxis 1
+        |> HostTensor.toList
+        |> List.indexed
+        |> List.sortBy snd
+        |> List.map fst
+        |> List.take (N.Shape.[0] - N.Shape.[1] |> int)
+        |> List.sort
+
+    let free =
+        [0 .. int N.Shape.[0]-1]
+        |> List.filter (fun r -> not (List.contains r dependants))
+
+    let getRows rowList (V: Tensor<_>) =
+        rowList
+        |> List.map (fun r -> V.[int64 r .. int64 r, *])
+        |> Tensor.concat 0
+    
+    let ND = getRows dependants N
+    let NF = getRows free N
+
+    let ID = getRows dependants I
+    let IF = getRows free I
+
+    printMat "Dependants ND" ND
+    printMat "Free       NF" NF
+
+    // Now write the dependants as a function of the free.
+    // This is done by solving the free for the basis factors and then inserting into the dependants.
+    let NFI, _, _ = LinAlg.integerInverse NF
+    let NDF = Tensor.convert<Rat> ND .* NFI
+
+    printMat "Dependants given Free NDF" NDF
+
+    // okay, what needs to be done??
+    // add modulo determination
+    
+    // okay how to determine modulos?
+    // dependants must be whole numbers so compute the LCM of all rows
+
+    // computing LCM of NDF
+    // Each free coordinate must be divisable by it.
+    let FD =
+        NDF
+        |> Tensor.foldAxis (fun l v -> lcm l v.Dnm) (HostTensor.scalar bigint.One) 1
+
+    printMat "Free divisibiltiy requirements FD" FD
+
+
+
+    let dimFaith =
+        [0 .. int nIn-1]
+        |> List.map (fun d ->
+            match free |> List.tryFind ((=) d), dependants |> List.tryFind ((=) d) with
+            | Some fd, None -> Free fd
+            | None, Some dd -> Dependant dd
+            | _ -> failwith "dimension cannot be both free and dependant")
+
+
+    // now what is the inverse doing?
+    // mapping to all coordinates actually
+    // 
+
+    let xLow = XRanges |> List.map fst |> HostTensor.ofList
+    let xHigh = XRanges |> List.map snd |> HostTensor.ofList
+
+    let XFreeId = HostTensor.identity (int64 free.Length)
+    let XFreeZeros = HostTensor.zeros [int64 free.Length; nOut]
+
+    let XFreeConstr = Tensor.concat 0 [XFreeId; NDF]
+    let YConstr = Tensor.concat 0 [XFreeZeros; ID - NDF .* IF]
+
+    // just fill out the record
+    let si = {
+        Solvability = S
+        XDimFaith   = dimFaith
+        XFreeToXDep = NDF
+        YToXDep     = ID - NDF .* IF 
+        XFreeToC1   = XFreeConstr
+        YToC1       = YConstr
+        CB1         = xLow
+        XFreeToC2   = XFreeConstr
+        YToC2       = YConstr
+        CB2         = xHigh
+        YToRDiv     = ID
+        RDiv        = FD
+    }
+    
+
+    printfn "SumInfo:\n%A" si
+
+
+        
+
+
+
+
+    ()
+
+
+let doSummation (si: SumInfo) =
+    // we want to perform a summation and for that we need to know the summation ranges
+    // to find the lower summation range what do we do?
+        
+    ()
+
+
+
+let testInequal () =
+    let A = [[ 3; 5; 7; 8]
+             [-5; 5; 2; 3]
+             [ 0; 1; 2; 3]
+             [ 1; 2; 3; 4]] |> HostTensor.ofList2D |> Tensor.convert<Rat>
+    let b = [2; 3; 4; 0] |> HostTensor.ofList |> Tensor.convert<Rat>
+
+    // let A = [[ 1;  0; 0]
+    //          [-1; -2; 0]
+    //          [ 1;  1; 0]
+    //          [ 1; -1; 0]
+    //          [ 0;  1; 0]
+    //          [ 2;  1; 1]]
+    //         |> HostTensor.ofList2D |> Tensor.convert<Rat>
+    // let b = [0; -6; 2; 3; 0; 0] |> HostTensor.ofList |> Tensor.convert<Rat>
+
+    printMat "A" A
+    printMat "b" b
+
+    let sol = FourierMotzkin.solve A b  
+    match sol with
+    | Some sol ->        
+        printfn "Solution exists:"
+        let rec printSol sol =
+            let j = FourierMotzkin.active sol
+            if j >= 0L then
+                let low, high = FourierMotzkin.range sol
+                printfn "%A <= x_%d <= %A" low j high
+                let value = 
+                    if low > Rat.NegInf then low
+                    elif high < Rat.PosInf then high
+                    else Rat.Zero
+                printfn "Setting x_%d = %A" j value
+                printSol (sol |> FourierMotzkin.subst value)
+        printSol sol        
+    | None ->
+        printfn "No solution exists."
+    ()
+
+
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [|"test1"|] -> test1()
+    | [|"test2"|] -> test2()
+    | [|"test3"|] -> test3()
+    | [|"test4"|] -> test4()
+    | [|"testRectTransform"|] -> testRectTransform()
+    | [|"test5"|] -> test5()
+    | [|"testInequal"|] -> testInequal()
+    | _ -> failwith "unknown"
+    0
+
+
