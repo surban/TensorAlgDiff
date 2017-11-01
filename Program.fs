2fd8d0bfd4a60ac003c8e789fcd809565f16d789
Sebastian Urban <surban@surban.net>
2017-11-01 22:03:07 +0100

    Demo

--- Program.fs
+++ Program.fs
@@ -1,28 +1,35 @@
-﻿
-open System
-open System.Numerics
-open PLplot
-open Tensor
-open Tensor.Algorithms
+﻿open Elements
 
-open OldTests
-open ConsumerTests
+let printAllDerivs fn =       
+    let dFns = Elements.derivFunc fn
+    let dInArg = "d" + fn.Name
+    printfn "Input: %A" fn
+    for KeyValue(v, dFn) in dFns do
+        printfn "Derivative of %s w.r.t. %s: %A" fn.Name v dFn
+
+
+let doDemo () =
+    let i, iSize = Elements.pos "i", 3L
+    let j, jSize = Elements.pos "j", 4L
+    let k, kSize = Elements.pos "k", 5L  // summation index
+
+    let dimNames = [i.Name; j.Name]
+    let dimSizes = Map [i.Name, iSize; j.Name, jSize]    
+
+    let argShapes = Map ["a",[iSize; kSize]; "b",[jSize; kSize]; "c",[iSize; iSize]; "d",[iSize + kSize]]
+
+    let a, b, c, d = Elements.arg "a", Elements.arg "b", Elements.arg "c", Elements.arg "d"
+    let summand = (a[i;k] + b[j;k])**2. * c[i;i] + (d[i+k])**3.
+    let expr = exp (- Elements.sumConstRng "k" 0L (kSize-1L) summand)
+    let func = Elements.func "f" dimNames dimSizes argShapes expr
+
+    printAllDerivs func         
 
 
 [<EntryPoint>]
 let main argv =
-    match argv with
-    | [|"test1"|] -> test1()
-    | [|"test2"|] -> test2()
-    | [|"test3"|] -> test3()
-    | [|"test4"|] -> test4()
-    | [|"testRectTransform"|] -> testRectTransform()
-    | [|"testConsumers"|] -> testConsumers()
-    | [|"testConsumers2"|] -> testConsumers2()
-    | [|"testInequal"|] -> testInequal()
-    | [|"testInequal2"|] -> testInequal2()
-    | [|"testElements1"|] -> testElements1()
-    | _ -> failwith "unknown"
+    doDemo ()
     0
+
 
 
